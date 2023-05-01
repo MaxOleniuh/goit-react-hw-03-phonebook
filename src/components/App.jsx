d8799ad03f9b19bee5b57dce06a1b50b6f9a0980
oleniuh <maxim.oleniukh@gmail.com>
2023-05-01 22:10:34 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,25 +5,28 @@ import List from './List/List';
 import Filter from './Filter/Filter';
 export class App extends Component {
   state = {
-    // contacts: [
-    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    // ],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
   componentDidMount() {
     const savedContacts = JSON.parse(localStorage.getItem("contacts"));
-    if (List) {
+    // if (List) {
+    //   this.setState({ contacts: [savedContacts] });
+    // console.log(savedContacts) 
+    // }
+    if (List < 0) {
       this.setState({ contacts: savedContacts });
     console.log(savedContacts) 
     }
   }
 
   componentDidUpdate(prevProps, prevState) {
-
     if (prevState.contacts !== this.state.contacts) {
       localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
     }
